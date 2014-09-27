f4f9a5f5b2f711e855046aea9dccbdf1e9b81723
Raúl Cumplido <raulcumplido@gmail.com>
2014-09-27 20:03:02 +0100

    Added join channel method and PING-PONG Responses to server

--- irc/session.py
+++ irc/session.py
@@ -8,6 +8,9 @@ messages = {
         'nickname': 'NICK',
         'username': 'USER',
         'join_channel': 'JOIN',
+        'away': 'AWAY',
+        'list': 'LIST',
+        'pong': 'PONG',
         }
 
 class IRCConnectionProtocol(asyncio.Protocol):
@@ -18,16 +21,25 @@ class IRCConnectionProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
         transport.write(self._generate_message('password', self._password))
-        transport.write(self._generate_message('nickname', self._user))
+        asyncio.wait(transport.write(self._generate_message('nickname', self._user)))
         my_intern_message = " ".join([self._user, '0 * :raulcd'])
-        transport.write(self._generate_message('username', my_intern_message))
+        asyncio.wait(transport.write(self._generate_message('username', my_intern_message)))
+        self.transport = transport
 
     def data_received(self, data):
-        print('data received: {}'.format(data.decode()))
+        message_received = data.decode()
+        print('data received: {}'.format(message_received))
+        if message_received.count('PING'):
+            self.transport.write(self._generate_message('pong', ''))
+
 
     def connection_lost(self, exc):
         print('server closed the connection')
         asyncio.get_event_loop().stop()
+
+    @asyncio.coroutine
+    def join_channel(self, channel):
+        self.transport.write(self._generate_message('join_channel', channel))
 
     def _generate_message(self, message_type, message):
         return bytes(" ".join([messages[message_type], message, "\n"]), 'UTF-8')
@@ -35,53 +47,18 @@ class IRCConnectionProtocol(asyncio.Protocol):
 class Session(object):
 
     def __init__(self, host, user, password, **kwargs):
-        irc_connection = IRCConnectionProtocol(user, password)
+        self.irc_connection = IRCConnectionProtocol(user, password)
         loop = asyncio.get_event_loop()
-        coro = loop.create_connection(lambda: irc_connection, host, 6667)
+        coro = loop.create_connection(lambda: self.irc_connection, host, 6667)
+        loop.run_until_complete(coro)
+        
+    def join(self, channel):
+        coro = self.irc_connection.join_channel(channel)
+        loop = asyncio.get_event_loop()
         loop.run_until_complete(coro)
         loop.run_forever()
+
+    def close(self):
+        loop = asyncio.get_event_loop()
         loop.close()
-"""
-        task = asyncio.Task(self._connect(host, user, password))
-        task.add_done_callback(self._client_connected)
-        loop.run_forever()
-        self._connected = task.done()
 
-    @asyncio.coroutine
-    def _connect(self, host, user, password, port=6667):
-        host_port = (host, port)
-        client_reader, client_writer = yield from asyncio.open_connection(host, port)
-        log.info("Connected to: {}:{}".format(host, port))
-        task = asyncio.Task(self._read_message(client_reader))
-        task.add_done_callback(self._read_message)
-        task = asyncio.Task(self._send_user_data(client_writer, user, password))
-        task.add_done_callback(self._client_connected)
-
-    @asyncio.coroutine
-    def _read_message(self, reader):
-        data = yield from reader.readline()
-        log.info("Received data: {}".format(data))
-        return reader
-
-    @asyncio.coroutine
-    def _send_user_data(self, writer, user, password):
-        writer.write(self._generate_message('password', password))
-        writer.write(self._generate_message('nickname', user))
-        my_intern_message = " ".join([user, '0 * :raulcd'])
-        writer.write(self._generate_message('username', my_intern_message))
-
-        
-    def _client_connected(self, task):
-        if task.exception():
-            raise task.exception()
-        self._connected = task.done() 
-
-    def join(self, channel):
-        self._send_join_channel_message(channel)
-
-    def _send_join_channel_message(self, channel):
-        self._my_socket.send(self._generate_message('join_channel', channel))
-        print(self._read_message())
-        while True:
-            self._read_message()
-"""
--- test_irc.py
+++ test_irc.py
@@ -1,10 +1,8 @@
 import irc
 
 def test_connection():
-    irc.session(host='irc.freenode.net', user='user', password='password')
-    1 == 1
+    irc.session(host='irc.freenode.net', user='telo', password='password')
 
-#def test_join_channel():
-#    session = irc.session(host='sinisalo.freenode.net', user='user', password='password')
-#    users = session.join('#python')
-#    assert users is not None
+def test_join_channel():
+    session = irc.session(host='sinisalo.freenode.net', user='xxxir', password='password')
+    session.join('#python')
